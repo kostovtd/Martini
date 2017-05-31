ec0a6ecce921ea96cd63d17fa8b43e0aa5328dac
Todor Kostov <kostovtd@gmail.com>
2017-05-31 10:47:41 +0300

    Dummy debug logs included for martini_sample.

--- martini-sample/src/main/java/com/kostovtd/martini_sample/MainActivity.java
+++ martini-sample/src/main/java/com/kostovtd/martini_sample/MainActivity.java
@@ -2,18 +2,36 @@ package com.kostovtd.martini_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 import com.kostovtd.martini.Martini;
 import com.kostovtd.martini.SmsListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Martini.with(this).addGateway("123123").start();
+        Martini.with(this)
+                .addGateway("123123")
+                .setSmsListener(new SmsListener() {
+                    @Override
+                    public void onSmsReceived(SmsMessage smsMessage) {
+                        Log.d(TAG, "onSmsReceived: " + smsMessage.getOriginatingAddress());
+                    }
+                })
+                .start();
     }
 
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Martini.with(this).stop();
+    }
 }
