678af23e7b4a83a610f2ff3b9b97b14824231e5b
Todor Kostov <kostovtd@gmail.com>
2017-06-02 11:17:50 +0300

    Imports optimization.

--- martini/src/main/java/com/kostovtd/martini/SmsBroadcastReceiver.java
+++ martini/src/main/java/com/kostovtd/martini/SmsBroadcastReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
